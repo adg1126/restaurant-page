d95905136f22f1c9a3bd228c517163422a7622cd
Diane998 <gutierrez.almiradyan@gmail.com>
2020-06-04 15:37:55 +0800

    formatted code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import _ from 'lodash';
 require('./styles.css');
-import {homeTab, hContent} from './modules/home.js'
-import {aboutTab, aContent} from './modules/about.js'
-import {menuTab, mContent} from './modules/menu.js'
-
+import { homeTab, hContent } from './modules/home.js';
+import { aboutTab, aContent } from './modules/about.js';
+import { menuTab, mContent } from './modules/menu.js';
 
 let content = document.getElementById('content');
 
@@ -33,7 +31,7 @@ tab2.textContent = 'Menu';
 
 navBar.append(restaurantName, tab0, tab1, tab2);
 
-let burger = document.createElement('burger');
+let burger = document.createElement('div');
 burger.classList.add('burger');
 for (let i = 1; i < 4; i++) burger.innerHTML += `<div class='line${i}'></div>`;
 content.appendChild(burger);
@@ -47,7 +45,6 @@ let content1 = document.createElement('div');
 content1.classList.add('content-1');
 header.appendChild(content1);
 
-
 let footer = document.createElement('footer');
 footer.classList.add('center');
 content.appendChild(footer);
@@ -59,36 +56,47 @@ footer.appendChild(p1);
 let tabItems = document.querySelectorAll('.tab-item');
 
 function selecItem(e) {
-    (e.target.id == 'tab-0') ?
-        ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) ?
-            (aContent.remove(), mContent.remove(), homeTab(content1), navBarSlide())
-        : (aContent.remove(), mContent.remove(), homeTab(content1))
-    : (e.target.id == 'tab-1') ?
-        ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) ?
-            (hContent.remove(), mContent.remove(), aboutTab(content1), navBarSlide())
-        : (hContent.remove(), mContent.remove(), aboutTab(content1))
-    :
-        ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) ?
-            (hContent.remove(), aContent.remove(), menuTab(content1), navBarSlide())
-        : (hContent.remove(), aContent.remove(), menuTab(content1));
-};
+  e.target.id == 'tab-0'
+    ? /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+      ? (aContent.remove(), mContent.remove(), homeTab(content1), navBarSlide())
+      : (aContent.remove(), mContent.remove(), homeTab(content1))
+    : e.target.id == 'tab-1'
+    ? /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+      ? (hContent.remove(),
+        mContent.remove(),
+        aboutTab(content1),
+        navBarSlide())
+      : (hContent.remove(), mContent.remove(), aboutTab(content1))
+    : /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ? (hContent.remove(), aContent.remove(), menuTab(content1), navBarSlide())
+    : (hContent.remove(), aContent.remove(), menuTab(content1));
+}
 
 homeTab(content1);
-tabItems.forEach(tab => tab.addEventListener('click', selecItem));
+tabItems.forEach((tab) => tab.addEventListener('click', selecItem));
 
+function navBarSlide() {
+  navBar.classList.toggle('nav-bar-active');
 
-function navBarSlide () {
-    navBar.classList.toggle('nav-bar-active');
+  tabItems.forEach((tab, index) => {
+    tab.style.animation
+      ? (tab.style.animation = '')
+      : (tab.style.animation = `tabItemFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`);
+  });
 
-    tabItems.forEach((tab, index) => {
-        (tab.style.animation) ? tab.style.animation = ''
-            : tab.style.animation = `tabItemFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-    });
+  burger.classList.toggle('x');
 
-    burger.classList.toggle('x');
-
-    (navBar.classList.contains('nav-bar-active')) ? content1.firstChild.style.display = 'none'
-        : setTimeout(() => content1.firstChild.style.display = 'block', 100);
-};
+  navBar.classList.contains('nav-bar-active')
+    ? (content1.firstChild.style.display = 'none')
+    : setTimeout(() => (content1.firstChild.style.display = 'block'), 100);
+}
 
 burger.addEventListener('click', navBarSlide);
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,10 +1,10 @@
 let mContent = document.createElement('div');
-mContent.classList.add('show')
+mContent.classList.add('show');
 mContent.setAttribute('id', 'menu');
 
 let title = document.createElement('div');
 title.classList.add('title');
-title.textContent = "Menu";
+title.textContent = 'Menu';
 
 let grid = document.createElement('div');
 grid.classList.add('grid');
@@ -14,7 +14,7 @@ appetizer.classList.add('appetizer');
 appetizer.textContent = 'Appetizer';
 
 let mainCourse = document.createElement('div');
-mainCourse.classList.add('main-course')
+mainCourse.classList.add('main-course');
 mainCourse.textContent = 'Main course';
 
 let dessert = document.createElement('div');
@@ -29,27 +29,32 @@ cell0.innerHTML += '<p>Green onions, vegetable oil</p>';
 let cell1 = document.createElement('div');
 cell1.classList.add('menu-item');
 cell1.textContent = 'Xiao Long Bao(Soup Dumplings)';
-cell1.innerHTML += '<p>Pork, gelatin, flour, ginger, garlic, soy sauce, broth, scallion, salt, sesame oil</p>';
+cell1.innerHTML +=
+  '<p>Pork, gelatin, flour, ginger, garlic, soy sauce, broth, scallion, salt, sesame oil</p>';
 
 let cell2 = document.createElement('div');
 cell2.classList.add('menu-item');
 cell2.textContent = 'Bao Bing(Shaved Ice)';
-cell2.innerHTML += '<p>Bao Bing topped with taro balls, mung beans and grass jelly</p>';
+cell2.innerHTML +=
+  '<p>Bao Bing topped with taro balls, mung beans and grass jelly</p>';
 
 let cell3 = document.createElement('div');
 cell3.classList.add('menu-item');
 cell3.textContent = 'Zong Zi(Sticky Rice Dumpling)';
-cell3.innerHTML += '<p>Pork belly, red bean paste, bamboo leaves, glutinous rice</p>';
+cell3.innerHTML +=
+  '<p>Pork belly, red bean paste, bamboo leaves, glutinous rice</p>';
 
 let cell4 = document.createElement('div');
 cell4.classList.add('menu-item');
 cell4.textContent = 'Gua Bao(Taiwanese Burger)';
-cell4.innerHTML += '<p>Pork belly, pickled mustard greens, steamed buns, five spice</p>';
+cell4.innerHTML +=
+  '<p>Pork belly, pickled mustard greens, steamed buns, five spice</p>';
 
 let cell5 = document.createElement('div');
 cell5.classList.add('menu-item');
 cell5.textContent = 'Yuebing(Moon Cake)';
-cell5.innerHTML += '<p>Salted egg yolk, water, golden syrup, lotus paste, cake</p>';
+cell5.innerHTML +=
+  '<p>Salted egg yolk, water, golden syrup, lotus paste, cake</p>';
 
 let cell6 = document.createElement('div');
 cell6.classList.add('menu-item');
@@ -66,10 +71,23 @@ cell8.classList.add('menu-item');
 cell8.textContent = 'Fengli Su(Pineaaple Cake)';
 cell8.innerHTML += '<p>Pineapple, eggs, sugar, flour, vanilla</p>';
 
-export {mContent};
+export { mContent };
 export let menuTab = (domElement) => {
-    mContent.style.display = 'block';
-    grid.append(appetizer, mainCourse, dessert, cell0, cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8);
-    mContent.append(title, grid);
-    domElement.append(mContent);
+  mContent.style.display = 'block';
+  grid.append(
+    appetizer,
+    mainCourse,
+    dessert,
+    cell0,
+    cell1,
+    cell2,
+    cell3,
+    cell4,
+    cell5,
+    cell6,
+    cell7,
+    cell8
+  );
+  mContent.append(title, grid);
+  domElement.append(mContent);
 };
